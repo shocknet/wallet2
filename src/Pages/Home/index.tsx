1de792075af1763a7d4c0bc0eb27656376aaad73
ruel121 <ruelavila121@gmail.com>
2022-11-18 06:14:38 -0500

    Complete UI of Nop Up page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 
-import { Filter } from "../../Containers/Filter";
-// import { Item } from "../../Components/Item";
-// import { Skeleton } from "../../Containers/Skeleton";
-import { ActionType, PageProps } from "../../globalTypes";
+import { PageProps } from "../../globalTypes";
 
 export const Home: React.FC<PageProps> = ({}): JSX.Element => {
 
--- src/Pages/NopeUp/index.tsx
+++ src/Pages/NopeUp/index.tsx
@@ -1,13 +1,25 @@
 import React from "react";
-
-import { Filter } from "../../Containers/Filter";
-// import { Item } from "../../Components/Item";
-// import { Skeleton } from "../../Containers/Skeleton";
-import { ActionType, PageProps } from "../../globalTypes";
+import { PageProps } from "../../globalTypes";
 
 export const NopeUp: React.FC<PageProps> = ({}): JSX.Element => {
 
   return(
-    <div>asdfasdfasd</div>
+    <div className="Home">
+      <div className="Home_title">Nope Up</div>
+      <div className="Home_textBox">
+        Bootstrap the wallet with a trusted server by chosing "Continue", you may add a node later<br/><br/><br/>
+        To add a node now, use "Manual Connection"
+      </div>
+      <div className="Home_manual">
+        <div className="Home_manual_text">
+          Manulal Connection
+        </div>
+        <div className="Home_manual_btn">
+          <button>
+            Continue
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
