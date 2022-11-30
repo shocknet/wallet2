28905d74f0055fdb1791099a6b6e4f666312b8a5
luckeyboy1004 <ruelavila121@gmail.com>
2022-11-30 15:17:20 +0200

    Add Items on Navigation list

--- src/Containers/Header/index.tsx
+++ src/Containers/Header/index.tsx
@@ -10,6 +10,12 @@ import Setting from "../../Assets/Icons/setting.svg";
 import SettingHover from "../../Assets/Icons/setting-hover.svg";
 import KeyHover from "../../Assets/Icons/key-hover.svg";
 import Key from "../../Assets/Icons/key.svg";
+import SourceIcon from "../../Assets/Icons/sourceIcon-white.svg";
+import SourceIconHover from "../../Assets/Icons/sourceIcon-hover.svg";
+import BuyCryptoIcon from "../../Assets/Icons/buyCrypto.svg";
+import BuyCryptoIconHover from "../../Assets/Icons/buyCrypto-hover.svg";
+import HelpAbout from "../../Assets/Icons/help&about.svg";
+import HelpAboutHover from "../../Assets/Icons/help&about-hover.svg";
 
 import { HeaderProps } from "./types";
 import { NavigateFunction, useNavigate } from "react-router-dom";
@@ -56,10 +62,33 @@ export const Header: React.FC<HeaderProps> = (): JSX.Element => {
           toggle();
         }}>
           <div className="Header_modal_content_item_img">
-            <img src={Setting} width="30px" alt="" />
-            <img src={SettingHover} width="30px" alt="" />
+            <img src={SourceIcon} width="30px" alt="" />
+            <img src={SourceIconHover} width="30px" alt="" />
           </div>
           <div className="Header_modal_content_item_text">Manage Sources</div>
+        </div>
+        <div className="Header_modal_content_item">
+          <hr />
+        </div>
+        <div className="Header_modal_content_item" onClick={() => {
+          navigate("/sources");
+          toggle();
+        }}>
+          <div className="Header_modal_content_item_img">
+            <img src={BuyCryptoIcon} width="30px" alt="" />
+            <img src={BuyCryptoIconHover} width="30px" alt="" />
+          </div>
+          <div className="Header_modal_content_item_text">Buy Bitcoin</div>
+        </div>
+        <div className="Header_modal_content_item" onClick={() => {
+          navigate("/sources");
+          toggle();
+        }}>
+          <div className="Header_modal_content_item_img">
+            <img src={HelpAbout} width="30px" alt="" />
+            <img src={HelpAboutHover} width="30px" alt="" />
+          </div>
+          <div className="Header_modal_content_item_text">Help/About</div>
         </div>
       </div>
     </div>
