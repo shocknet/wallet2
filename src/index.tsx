5fecafaa187c0d066bcb9d3c38c3cd011114eff5
luckyboy1004 <expert.vetted1024@gmail.com>
2023-08-25 23:48:58 +0200

    installable PWA

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,6 +10,9 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+//For install app function, ServiceWorker is registered
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
