713c5289e8440b5375877d2724230415ad7d2004
luckyboy1004 <expert.vetted1024@gmail.com>
2023-09-14 15:55:53 +0200

    update logo, redirect to home

--- src/Pages/NodeUp/index.tsx
+++ src/Pages/NodeUp/index.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigateFunction, useNavigate } from "react-router-dom";
 import { PageProps } from "../../globalTypes";
+import { useSelector, useDispatch } from 'react-redux';
 
 export const NodeUp: React.FC<PageProps> = (): JSX.Element => {
+
+
+  //declaration about reducer
+  const paySources = useSelector((state:any) => state.paySource).map((e:any)=>{return {...e}});
+  
+  const spendSources = useSelector((state:any) => state.spendSource).map((e:any)=>{return {...e}});
 
   const navigate: NavigateFunction = useNavigate();
 
@@ -14,6 +21,12 @@ export const NodeUp: React.FC<PageProps> = (): JSX.Element => {
       navigate("/loader")
     }
   };
+
+  useEffect(() => {
+    if(paySources.length!==0||spendSources.length!==0){
+      navigate("/home")
+    }
+  }, []);
 
   return(
     <div className="NodeUp">
