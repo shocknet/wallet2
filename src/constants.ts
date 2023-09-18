ba752e4dc6f6c335f82bfb11e78bf44b559f8774
boufni95 <boufni95@gmail.com>
2023-09-18 19:06:22 +0200

    update with mock-pub key

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,6 @@ export const locationRegex = new RegExp(/\w{1,}/g)
 export const HTTP_AUTH_TOKEN_STORAGE_KEY = "HTTP_AUTH_TOKEN"
 export const NOSTR_PRIVATE_KEY_STORAGE_KEY = "NOSTR_PRIVATE_KEY"
 export const NOSTR_RELAYS = ["wss://relay.unikku.com"]
-export const NOSTR_PUB_DESTINATION = "7cb96be9b719b694ac8a8d48c642381a4cbcf41657d2cb4c242769aa0f8c57f5";
+export const NOSTR_PUB_DESTINATION = "e306c45ee0a7c772540f1dc88b00f79d2d3910bfd4047e910584998de9c9e2be";
 export const usdToBTCBuyLink = "https://api.coinbase.com/v2/prices/BTC-USD/buy";
 export const usdToBTCSellLink = "https://api.coinbase.com/v2/prices/BTC-USD/sell";
