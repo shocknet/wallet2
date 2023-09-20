9cdd21dadc1a9cf1a1a52abcf308fd9c688fee1d
luckyboy1004 <expert.vetted1024@gmail.com>
2023-09-20 00:16:08 -0700

    cache a keypair

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,7 @@ export const locationRegex = new RegExp(/\w{1,}/g)
 
 export const HTTP_AUTH_TOKEN_STORAGE_KEY = "HTTP_AUTH_TOKEN"
 export const NOSTR_PRIVATE_KEY_STORAGE_KEY = "NOSTR_PRIVATE_KEY"
+export const NOSTR_PUBLIC_KEY_STORAGE_KEY = "NOSTR_PUBLIC_KEY"
 export const NOSTR_RELAYS = ["wss://relay.unikku.com"]
 export const NOSTR_PUB_DESTINATION = "e306c45ee0a7c772540f1dc88b00f79d2d3910bfd4047e910584998de9c9e2be";
 export const usdToBTCBuyLink = "https://api.coinbase.com/v2/prices/BTC-USD/buy";
