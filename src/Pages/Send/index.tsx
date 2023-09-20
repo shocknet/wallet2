0a9b055320f0f055752494e5d0376e4f60d7f56a
luckyboy1004 <expert.vetted1024@gmail.com>
2023-09-20 09:29:13 -0700

    update pay functioin

--- src/Pages/Send/index.tsx
+++ src/Pages/Send/index.tsx
@@ -12,7 +12,7 @@ import type { NotificationPlacement } from 'antd/es/notification/interface';
 import axios from 'axios';
 import { useIonRouter } from '@ionic/react';
 import { Modal } from '../../components/Modals/Modal';
-import SpendFromDropdown from '../../components/Dropdowns/SpendFromDropdown';
+import SpendFromDropdown from '../../Components/Dropdowns/SpendFromDropdown';
 
 type PayInvoice = {
   type: 'payInvoice'
@@ -167,8 +167,8 @@ export const Send = () => {
       case 'payAddress':
         const resA = await nostr.PayAddress({
           address: action.address,
-          amoutSats: amountToPay,
-          targetConf: 10
+          amoutSats: +amount,
+          satsPerVByte: 10
         })
         if (resA.status !== 'OK') {
           setError(resA.reason)
