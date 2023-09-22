eeaa8ed8891ad2b3ffb4a6e4db46d6286f966659
luckyboy1004 <expert.vetted1024@gmail.com>
2023-09-22 15:56:39 -0700

    prefs page update UI

--- src/Pages/Prefs/index.tsx
+++ src/Pages/Prefs/index.tsx
@@ -1,19 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { PageProps } from "../../globalTypes";
 
 import { useSelector, useDispatch } from 'react-redux';
-import axios from 'axios';
 import { useIonRouter } from '@ionic/react';
-
-type PayInvoice = {
-  type: 'payInvoice'
-  invoice: string
-  amount: number
-}
-type PayAddress = {
-  type: 'payAddress'
-  address: string
-}
+import * as Icons from "../../Assets/SvgIconLibrary";
 
 export const Prefs = () => {
   const price = useSelector((state:any) => state.usdToBTC);
@@ -28,10 +17,48 @@ export const Prefs = () => {
   });
 
   return (
-    <div className='Send_container'>
-      <div className="Send">
-        <div className="Send_header_text">Preferences</div>
-        
+    <div className='Prefs_container'>
+      <div className="Prefs">
+        <div className="Prefs_header_text">Preferences</div>
+        <div className="Prefs_chainfee">
+          <header>Default Chain Fee</header>
+          <div className='Prefs_chainfee_options'>
+            <div className='Prefs_chainfee_options_first'>
+              <p className='Prefs_chainfee_options_top'>Economy</p>
+              <p className='Prefs_chainfee_options_bottom'>10 sat/byte</p>
+            </div>
+            <div className='Prefs_chainfee_options_second'>
+              <p className='Prefs_chainfee_options_top'>Average</p>
+              <p className='Prefs_chainfee_options_bottom'>30 sat/byte</p>
+            </div>
+            <div className='Prefs_chainfee_options_third'>
+              <p className='Prefs_chainfee_options_top'>ASAP</p>
+              <p className='Prefs_chainfee_options_bottom'>60 sat/byte</p>
+            </div>
+          </div>
+          <div className='Prefs_chainfee_settings'>
+            {Icons.prefsSetting()}
+          </div>
+        </div>
+        <div className='Prefs_mempool'>
+          <header>Mempool Provider</header>
+          <input type="text" placeholder="Invoice, Bitcoin or Lightning Address, nPub, Email"/>
+        </div>
+        <div className='Prefs_fiat'>
+          <header>Fiat Estimates</header>
+          <div className='Prefs_fiat_estimates'>
+            <select className='Prefs_fiat_currency'>
+              <option value={"usd"}>USD</option>
+              <option value={"eur"}>EUR</option>
+              <option value={"cny"}>CNY</option>
+            </select>
+            <input type="text" placeholder="Invoice, Bitcoin or Lightning Address, nPub, Email"/>
+          </div>
+        </div>
+        <div className='Prefs_buttons'>
+          <button className='Prefs_buttons_cancel'>Cancel</button>
+          <button className='Prefs_buttons_save'>Save</button>
+        </div>
       </div>
     </div>
   )
