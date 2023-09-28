dc4230a0c25353a0069fc268cb0ed9e5a8dc7e09
luckyboy1004 <expert.vetted1024@gmail.com>
2023-09-29 01:00:02 +0300

    add to bitcoin protocol to android

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import { usdToBTCSpotLink } from "../constants";
 import { useDispatch } from "react-redux";
 import { setAmount } from "../State/Slices/usdToBTCSlice";
+import { App } from "@capacitor/app";
 
 interface Price {
   buyPrice: number,
@@ -17,22 +18,8 @@ export const Layout: React.FC<LayoutProps> = ({ children }): JSX.Element => {
   const dispatch = useDispatch();
   
   const getPrice = async () => {
-    const buyInfo = await axios.get<any>(
-      usdToBTCSpotLink,
-      {
-        headers: {
-          Accept: 'application/json',
-        }
-      }
-    );
-    const sellInfo = await axios.get<any>(
-      usdToBTCSpotLink,
-      {
-        headers: {
-          Accept: 'application/json',
-        }
-      }
-    );
+    const buyInfo = await axios.get<any>(usdToBTCSpotLink);
+    const sellInfo = await axios.get<any>(usdToBTCSpotLink);
 
     dispatch(setAmount(
       {
@@ -44,7 +31,23 @@ export const Layout: React.FC<LayoutProps> = ({ children }): JSX.Element => {
 
   useEffect(()=>{
     getPrice();
-  })
+  });
+
+  useEffect(() => {
+      // Listen for the appUrlOpen event
+      const listener = App.addListener("appUrlOpen", (data) => {
+        console.log("appurlopen");
+        
+      // Get the bitcoin URI from the data
+      const bitcoinUri = data.url;
+      // Do something with the bitcoin URI, such as parsing it or sending it to another component
+    });
+    
+    // Remove the listener when the component is unmounted
+    return () => {
+      listener.remove();
+    };
+  }, []);
 
   return(
     <React.Fragment>
