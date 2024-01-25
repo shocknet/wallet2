ebbff527b3c1faf50bd3e2bd96440c4ad1c718f5
luckyboy1004 <leo.champlin2022gmail.com>
2024-01-25 09:00:54 -0400

    working on deep link

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { Route } from 'react-router-dom';
 import {
   IonApp,
   IonRouterOutlet,
-  setupIonicReact
+  setupIonicReact,
+  useIonRouter
 } from '@ionic/react';
 import { IonReactHashRouter } from '@ionic/react-router';
 import { StatusBar } from '@capacitor/status-bar';
 import AppUrlListener from './Hooks/appUrlListener';
-import { DeeplinkService } from './Hooks/deeplink.service';
+import { DeeplinkService, isAppInstalled, useInstallAppAlert } from './Hooks/deeplink.service';
 
 /* Core CSS required for Ionic components to work properly */
 // import '@ionic/react/css/core.css';
@@ -51,9 +52,17 @@ import { Metrics } from './Pages/Metrics';
 setupIonicReact();
 
 const App: React.FC = () => {
+  const router = useIonRouter();
+
   // deep linking handler to open App
-  const handleDeepLink = (link: string = '') => {
-    DeeplinkService(link);
+  const handleDeepLink = async (link: string = '') => {
+    const appInstalled = await isAppInstalled();
+    if(appInstalled) {
+      DeeplinkService(link);
+    } else {
+      const showAlert: string = useInstallAppAlert();
+      router.push(showAlert);
+    }
   }
 
   useEffect(() => {
@@ -61,9 +70,8 @@ const App: React.FC = () => {
     // call deep link
     setTimeout(() => {
       handleDeepLink();
-      
     }, 1000);
-  }, []);
+  }, [router]);
 
   const setStatusBarColor = async () => {
     await StatusBar.setBackgroundColor({ color: '#16191c' }); // Replace with your desired color code
--- src/Hooks/deeplink.service.ts
+++ src/Hooks/deeplink.service.ts
@@ -8,66 +8,49 @@ const { Linking } = Plugins;
 const appLink = `${appName}://open`; // get app name
 
 export const DeeplinkService = async (link: string) => {
-    const appInstalled = await isAppInstalled(); // check app installation
+    const router = useIonRouter();
+    router.push(`https://apps.apple.com/app/${appId}`)
+    // const appInstalled = await isAppInstalled(); // check app installation
     alert('start deep linking')
-    if(appInstalled) {
-        alert('app is installed')
-        const incomingURL = navigator.userAgent.toLowerCase(); // decode incoming URL
-        const isAndroid = incomingURL.indexOf("android") > -1; // android check
-        const isIphone = incomingURL.indexOf("iphone") > -1; // ios check
+    alert('app is installed')
+    const incomingURL = navigator.userAgent.toLowerCase(); // decode incoming URL
+    const isAndroid = incomingURL.indexOf("android") > -1; // android check
+    const isIphone = incomingURL.indexOf("iphone") > -1; // ios check
 
-        const openApp = async () => {
-            await Linking.openUrl(appLink);
-        };
+    const openApp = async () => {
+        await Linking.openUrl(appLink);
+    };
 
-        if (isIphone) {
-            alert('you are in iphone')
-            openApp();
-        } else if (isAndroid) {
-            alert('you are in android')
-        } else{
-            alert('you are in desktop')
-        }
-    } else {
-        alert('app is not installed')
-        useInstallAppAlert();
+    if (isIphone) {
+        alert('you are in iphone')
+        openApp();
+    } else if (isAndroid) {
+        alert('you are in android')
+    } else{
+        alert('you are in desktop')
     }
 }
 
 // in case app is not installed yet, it shows users alert to install the app from app store
-const useInstallAppAlert = () => {
-    const [presentAlert] = useIonAlert();
-    const router = useIonRouter();
-    
+export const useInstallAppAlert = () => {
+    const appStoreURL = `https://apps.apple.com/app/${appId}`;
+
     // show alert to install the app
-    presentAlert({
-        header: 'App Not Installed',
-        message: 'To use this feature, please install our app.',
-        buttons: [
-          {
-            text: 'Install App',
-            handler: () => {
-              openAppStore();
-            },
-          },
-          'Cancel',
-        ],
-    });
-  
-    const openAppStore = () => {
-        const appStoreURL = `https://apps.apple.com/app/${appId}`;
-        router.push(appStoreURL);
-    };
+    const confirm = window.confirm("App Not Installed, To use this feature, please install our app.");
+    if(confirm) {
+        return appStoreURL;
+    } else {
+        return '/#';
+    }
 };
 
 // check wonder the app is installed
-const isAppInstalled = async (): Promise<boolean> => {
-    const customScheme = appLink; // Replace with your actual custom URL scheme
+export const isAppInstalled = async (): Promise<boolean> => {
     try {
-      await Linking.openUrl(customScheme);
+      await Linking.openUrl(appLink);
       return true;
     } catch (error) {
       console.log('Your app is not installed.');
     }
     return false;
-  };
+};
