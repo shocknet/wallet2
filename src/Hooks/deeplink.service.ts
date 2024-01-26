443003ae6d972b2dff677f1a5a598e395a439a95
luckyboy1004 <leo.champlin2022@gmail.com>
2024-01-26 14:20:40 -0600

    implemented deep linking functionaltiy

--- src/Hooks/deeplink.service.ts
+++ src/Hooks/deeplink.service.ts
@@ -12,61 +12,8 @@ export const DeeplinkService = (link: string) => {
     // Linking.openUrl.App.openUrl('tg://')
     const isiPhone = /iPhone|iPod/.test(navigator.platform);
     if(isiPhone) {
-        // window.location.href = appLink.toLowerCase();
-        window.open(appLink.toLowerCase(), '_self')
+        window.location.href = appLink.toLowerCase();
+        // window.open(appLink.toLowerCase(), '_self')
         // window.location.href = 'https://test.shockwallet.app/open/';
     }
-    // try {
-    //     window.location.href = appLink;
-    //     return true;
-    // } catch (error) {
-    //     alert('asdfasdf')
-    //     return false;
-    // }
-    // // window.open('tgd://', '_self')
-    // if(appInstalled) {
-    //     alert('app is installed')
-    //     const incomingURL = navigator.userAgent.toLowerCase(); // decode incoming URL
-    //     const isAndroid = incomingURL.indexOf("android") > -1; // android check
-    //     const isIphone = incomingURL.indexOf("iphone") > -1; // ios check
-
-    //     const openApp = async () => {
-    //         await Linking.openUrl(appLink);
-    //     };
-
-    //     if (isIphone) {
-    //         alert('you are in iphone')
-    //         openApp();
-    //     } else if (isAndroid) {
-    //         alert('you are in android')
-    //     } else{
-    //         alert('you are in desktop')
-    //     }
-    // } else {
-    //     alert('app is not installed')
-    //     useInstallAppAlert();
-    // }
 }
-
-// // in case app is not installed yet, it shows users alert to install the app from app store
-// const useInstallAppAlert = async () => {
-//     const openAppStore = () => {
-//         const appStoreURL = `https://apps.apple.com/app/${appId}`;
-//         Linking.openUrl(appStoreURL);
-//     };
-
-//     // show alert to install the app
-//     if (window.confirm("App Not Installed, To use this feature, please install our app.")) {
-//         openAppStore();
-//     }
-// };
-
-// // check wonder the app is installed
-// const isAppInstalled = async (): Promise<boolean> => {
-//     try {
-//       await Linking.openUrl('tg://');
-//       return true;
-//     } catch (error) {
-//         return false;
-//     }
-// };
