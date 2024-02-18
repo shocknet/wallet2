8b4bd1b56f9c6d2e58bec5797332e3c1a6a45b62
luckyboy1004 <leo.champlin2022@gmail.com>
2024-02-18 23:46:56 +0100

    updated prebuild.js for android

--- preBuild.js
+++ preBuild.js
@@ -1,10 +1,23 @@
 // preBuild.js
 const fs = require('fs');
 const path = require('path');
+
+// Read environment variables
 require('dotenv').config({ path: '.env.development' });
 
-// Replace the appUrl placeholder in the build.gradle file with the actual value
-const buildGradlePath = path.join(__dirname, 'android', 'app', 'build.gradle');
-let buildGradle = fs.readFileSync(buildGradlePath, 'utf8');
-buildGradle = buildGradle.replace(/appUrl\s.*/, `appUrl "${process.env.VITE_APP_URL}"`);
-fs.writeFileSync(buildGradlePath, buildGradle);
+// Path to the strings.xml file
+const stringsXmlPath = path.resolve(__dirname, 'android', 'app', 'src', 'main', 'res', 'values', 'strings.xml');
+
+// Read the existing strings.xml content
+const stringsXmlContent = fs.readFileSync(stringsXmlPath, 'utf-8');
+
+// Update the "app_url" string in the strings.xml content
+const updatedStringsXmlContent = stringsXmlContent.replace(
+  /<string name="app_url">.*?<\/string>/,
+  `<string name="app_url">${process.env.VITE_APP_URL}</string>`
+);
+
+// Write the updated content back to the strings.xml file
+fs.writeFileSync(stringsXmlPath, updatedStringsXmlContent, 'utf-8');
+
+console.log('strings.xml "app_url" line updated with the environment-specific value.');
