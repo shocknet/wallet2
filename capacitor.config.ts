c4df8dbf19e041672b6578021ce45d4aa19fa5c2
luckyboy1004 <leo.champlin2022@gmail.com>
2024-02-18 18:29:09 -0600

    updated android build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.shockwallet',
+  appId: 'app.shockwallet.test',
   appName: 'SHOCKWALLET',
   webDir: 'dist',
   server: {
