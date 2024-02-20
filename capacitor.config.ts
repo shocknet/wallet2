5efd007e1123fce46e8d0248755b011832962e04
luckyboy1004 <leo.champlin2022@gmail.com>
2024-02-20 18:57:14 +0100

    reversed app id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.shockwallet.test',
+  appId: 'app.shockwallet',
   appName: 'SHOCKWALLET',
   webDir: 'dist',
   server: {
