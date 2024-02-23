899db71241858b1e473c0b15b11bc7ea376d7e16
luckyboy1004 <leo.champlin2022@gmail.com>
2024-02-23 19:46:14 +0100

    updated prebuild script

--- preBuild/preBuild.ts
+++ preBuild/preBuild.ts
@@ -1,16 +1,18 @@
 // preBuild.js
 const fs = require('fs');
-require('dotenv').config({ path: '.env.development' });
+require('dotenv').config();
 
 // Read and modify the AndroidManifest.xml file
 const androidManifestPath = 'android/app/src/main/AndroidManifest.xml';
-let androidManifest = fs.readFileSync(androidManifestPath, 'utf8');
+let androidManifest = fs.readFileSync('preBuild/AndroidManifest.copy.xml', 'utf8');
 androidManifest = androidManifest.replace('${appUrl}', process.env.VITE_APP_URL);
 fs.writeFileSync(androidManifestPath, androidManifest);
 
 // Read and modify the Info.plist file
 const infoPlistPath = 'ios/App/App/Info.plist';
-let infoPlist = fs.readFileSync(infoPlistPath, 'utf8');
+let infoPlist = fs.readFileSync('preBuild/Info.copy.plist', 'utf8');
+console.log(infoPlist);
+
 infoPlist = infoPlist.replace('${appUrl}', process.env.VITE_APP_URL);
 fs.writeFileSync(infoPlistPath, infoPlist);
 
