c549e0783d946fe59db1063a8e9b2dcfecdee5fb
boufni95 <boufni95@gmail.com>
2024-05-09 19:16:06 +0200

    fetch beacon

--- src/Components/BackgroundJobs/HealthCheck.tsx
+++ src/Components/BackgroundJobs/HealthCheck.tsx
@@ -3,7 +3,9 @@ import { getAllNostrClients, nostrCallback } from "../../Api/nostr"
 import { toast } from "react-toastify";
 import Toast from "../Toast";
 
-const SubsCheckIntervalSeconds = 20
+const SubsCheckIntervalMs = 20 * 1000
+const SubsThresholdMs = 10 * 1000
+const BeaconMaxAgeSeconds = 2 * 60
 export const HealthCheck = () => {
     // const paySource = useSelector(({ paySource }) => paySource)
     // const spendSource = useSelector(({ spendSource }) => spendSource)
@@ -12,11 +14,12 @@ export const HealthCheck = () => {
     useEffect(() => {
         const interval = setInterval(() => {
             checkHealth()
-        }, SubsCheckIntervalSeconds * 1000)
+        }, SubsCheckIntervalMs)
         return () => {
             clearInterval(interval)
         }
     }, [])
+
 
     const checkHealth = () => {
         getAllNostrClients().forEach((wrapper) => {
@@ -33,27 +36,21 @@ export const HealthCheck = () => {
             }
             const now = Date.now()
             const startedAtMillis = (oldestSingleSub as nostrCallback<any>).startedAtMillis
-            if (now - startedAtMillis < 10 * 1000) {
-                console.log("oldest single sub is less than 10 seconds old")
+            if (now - startedAtMillis < SubsThresholdMs) {
+                console.log("oldest single sub is less than ", SubsThresholdMs, " seconds old")
                 return
             }
             console.log("oldest sub is", (startedAtMillis - now) / 1000, "seconds old!")
-            if (now - state.latestResponseAtMillis < 10 * 1000) {
-                console.log("latest response is less than 10 seconds old")
+            if (now - state.latestResponseAtMillis <= SubsThresholdMs) {
+                console.log("latest response is less than ", SubsThresholdMs, " seconds old")
                 return
             }
-            if (state.latestHelthReqAtMillis <= state.latestResponseAtMillis) {
-                console.log("no health req was sent since last response, sending health req")
-                wrapper.sendHelthRequest()
-                return
-            }
-            if (now - state.latestHelthReqAtMillis < 10 * 1000) {
-                console.log("latest health req is less than 10 seconds old")
-                return
-            }
-            console.log("no response for more than 10 seconds, disconnecting")
-            wrapper.disconnectCalls()
-            toast.error(<Toast title="Source Error" message={`Cannot connect to source: ${wrapper.getPubDst().slice(0, 10)}.`} />)
+            console.log("latest response is more than ", SubsThresholdMs, " seconds old, checking beacon state")
+            wrapper.checkBeaconHealth(BeaconMaxAgeSeconds, () => {
+                console.log("service is down, beacon is older than", BeaconMaxAgeSeconds, "seconds, disconnecting")
+                wrapper.disconnectCalls()
+                toast.error(<Toast title="Source Error" message={`Cannot connect to source: ${wrapper.getPubDst().slice(0, 10)}.`} />)
+            })
         })
     }
     /*
