b8c06ad3495ab0b257145eea662e54a6ed7709e6
boufni95 <boufni95@gmail.com>
2024-07-23 22:43:59 +0200

    robo icons for sources

--- src/Components/Dropdowns/SpendFromDropdown/index.tsx
+++ src/Components/Dropdowns/SpendFromDropdown/index.tsx
@@ -21,15 +21,15 @@ const SpendFromDropdown: React.FC<DropDownProps> = ({
 
   useEffect(() => {
     setShowDropDown(showDropDown);
-    const box = allValue.filter((e) => e.id !== value.id );
+    const box = allValue.filter((e) => e.id !== value.id);
     setAllValue(box);
   }, [showDropDown]);
 
-  const arrangeIcon = (value?: string) => {
+  const arrangeIcon = (value?: string, sourcePub?: string) => {
     switch (value) {
       case "0":
         return <React.Fragment>
-          <img src = {BootstrapSource} width="23px" alt='Avatar' style={{borderRadius: "50%"}}/>
+          <img src={BootstrapSource} width="23px" alt='Avatar' style={{ borderRadius: "50%" }} />
         </React.Fragment>
       case "1":
         return icons.mynodeSmall()
@@ -45,25 +45,30 @@ const SpendFromDropdown: React.FC<DropDownProps> = ({
 
       case "5":
         return icons.stackerSmall()
-          
+
       default:
+        if (sourcePub) {
+          return <React.Fragment>
+            <img src={`https://robohash.org/${sourcePub}.png?bgset=bg1`} width="33px" alt='Avatar' style={{ borderRadius: "50%" }} />
+          </React.Fragment>
+        }
         if (!value?.includes("http")) {
-          value = "http://www.google.com/s2/favicons?sz=64&domain="+value;
+          value = "http://www.google.com/s2/favicons?sz=64&domain=" + value;
         }
         return <React.Fragment>
-          <img src = {value} width="23px" alt='' style={{borderRadius: "50%"}}/>
+          <img src={value} width="23px" alt='' style={{ borderRadius: "50%" }} />
         </React.Fragment>
     }
   }
 
   const dropdown = () => {
-    setDisplay( display === 0 ? 1 : 0 );
-    setRotation( rotation === 0 ? 90 : 0 )
+    setDisplay(display === 0 ? 1 : 0);
+    setRotation(rotation === 0 ? 90 : 0)
   }
 
   const selectOption = (id?: string) => {
-    const selected = values.filter((e) => e.id === id );
-    const remainValues = values.filter((e) => e.id !== id );
+    const selected = values.filter((e) => e.id === id);
+    const remainValues = values.filter((e) => e.id !== id);
     setAllValue(remainValues);
     dropdown();
     callback(selected[0])
@@ -73,27 +78,28 @@ const SpendFromDropdown: React.FC<DropDownProps> = ({
     <>
       <div className={showDropDown ? 'spend_from' : 'spend_from active'}>
         {
-          value?
+          value ?
             (<div className="spend_from_item" key={value.id}>
               <div className="spend_from_item_left">
-                <div className="spend_from_item_icon">{arrangeIcon(value.icon)}</div>
+                <div className="spend_from_item_icon">{arrangeIcon(value.icon, value.pubSource ? value.id.split("-")[0] : undefined)}</div>
                 <div className="spend_from_item_input">
-                  <div style={{width: "130px"}}>{value.label}</div>
+                  <div style={{ width: "130px" }}>{value.label}</div>
                 </div>
               </div>
               <div className="spend_from_item_balance">{value.balance}</div>
             </div>)
-          :
+            :
             <div>
             </div>
         }
-        <div className="spend_from_dropdown" style={{opacity: display, transition: "0.3s", overflow: "hidden"}}>
+        <div className="spend_from_dropdown" style={{ opacity: display, transition: "0.3s", overflow: "hidden" }}>
           {display === 1 && allValue.map(
             (item: SpendFrom) => {
+              const [sourcePub] = item.id.split("-");
               return (
-                <div onClick={() => {selectOption(item.id)}} className="spend_from_item" key={item.id}>
+                <div onClick={() => { selectOption(item.id) }} className="spend_from_item" key={item.id}>
                   <div className="spend_from_item_left">
-                    <div className="spend_from_item_icon">{arrangeIcon(item.icon)}</div>
+                    <div className="spend_from_item_icon">{arrangeIcon(item.icon, item.pubSource ? sourcePub : undefined)}</div>
                     <div className="spend_from_item_input">
                       <div>{item.label}</div>
                     </div>
@@ -105,7 +111,7 @@ const SpendFromDropdown: React.FC<DropDownProps> = ({
           )}
         </div>
       </div>
-      <button className="spend_from_toggle" onClick={dropdown} style={{transform: `rotate(${rotation}deg)`, transition: "0.3s"}}>
+      <button className="spend_from_toggle" onClick={dropdown} style={{ transform: `rotate(${rotation}deg)`, transition: "0.3s" }}>
         {icons.arrow()}
       </button>
     </>
