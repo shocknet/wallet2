cd00ff5fd39a41db924e7aa72a31b221e17d6681
polarDender <patch.demon115@gmail.com>
2024-08-08 15:30:50 -0700

    feat-complete-cature

--- src/Pages/Feat/index.tsx
+++ src/Pages/Feat/index.tsx
@@ -25,32 +25,62 @@ export const Feat = () => {
   ]);
   const [file, setFile] = useState("");
   const [scroll, setScroll] = useState(0);
+  const [firstKey, setfirstKey] = useState("");
 
   useEffect(() => {
     const scrollField = document.getElementById("HistoryFeild");
     scrollField?.scrollTo(0, scroll);
   }, [scroll]);
 
-  const sendMessage = () => {
+  const sendMessage = (selected: string) => {
+    console.log(selected);
     const scrollField = document.getElementById("HistoryFeild");
     setScroll(scrollField.scrollHeight);
-    if (file) {
+    if (selected == "normal") {
+      if (file) {
+        messageHistory.push({
+          time_stamp: Date.now(),
+          chat: message,
+          msg_type: "send_image",
+          file: file,
+        });
+      } else {
+        messageHistory.push({
+          time_stamp: Date.now(),
+          chat: message,
+          msg_type: "send_message",
+          file: "",
+        });
+      }
+    }
+
+    if (selected == "/Send Payment") {
       messageHistory.push({
         time_stamp: Date.now(),
-        chat: message,
-        msg_type: "send_image",
-        file: file,
+        chat: selected,
+        msg_type: "send_message",
+        file: "",
       });
-    } else {
+    }
+
+    if (selected == "/Create a Cloude Node") {
       messageHistory.push({
         time_stamp: Date.now(),
-        chat: message,
+        chat: selected,
+        msg_type: "send_message",
+        file: "",
+      });
+    }
+
+    if (selected == "/Is this private?") {
+      messageHistory.push({
+        time_stamp: Date.now(),
+        chat: selected,
         msg_type: "send_message",
         file: "",
       });
     }
     setMessageHistory(messageHistory);
-    // scroll_bottom();
     setEditing(false);
     setMessage("");
     setFile("");
@@ -83,6 +113,16 @@ export const Feat = () => {
         id="message"
         className="Message"
         onChange={(e) => editingMessage(e.target.value)}
+        onKeyDownCapture={(e) => {
+          if (e.key == "Control") setfirstKey("Control");
+          if (e.key == "v") {
+            if (firstKey == "Control") {
+              console.log("copy");
+              pastImage();
+              setfirstKey("");
+            }
+          }
+        }}
         value={message}
       />
       <div className="Feat_inputFile">
@@ -103,45 +143,84 @@ export const Feat = () => {
     </div>
   );
 
-  const sendIcon = (
-    <div className="Feat_sendIcon">
-      <input
-        type="text"
-        placeholder="Message"
-        id="message"
-        className="Message"
-        onChange={(e) => editingMessage(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key == "Enter") {
-            sendMessage();
-          }
-        }}
-        value={message}
-      />
-      <button onClick={sendMessage}>{Icons.sendIcon()}</button>
-    </div>
-  );
+  const sendIcon = () => {
+    if (file) {
+      return (
+        <div className="Feat_sendIcon">
+          <input
+            type="text"
+            placeholder="Message"
+            id="message"
+            className="Message"
+            onChange={(e) => editingMessage(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key == "Enter") {
+                sendMessage("normal");
+              }
+            }}
+            value={message}
+          />
+          <div className="Feat_selectImage">
+            <img src={file} alt="image" />
+          </div>
+          <button onClick={() => sendMessage("normal")}>
+            {Icons.sendIcon()}
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="Feat_sendIcon">
+          <input
+            type="text"
+            placeholder="Message"
+            id="message"
+            className="Message"
+            onChange={(e) => editingMessage(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key == "Enter") {
+                sendMessage("normal");
+              }
+            }}
+            value={message}
+          />
+          <div className="Feat_selectImage">
+          </div>
+          <button onClick={() => sendMessage("normal")}>
+            {Icons.sendIcon()}
+          </button>
+        </div>
+      );
+    }
+  };
 
   const send_payment = () => {
-    editingMessage("/Send Payment");
-    document.getElementById("message")?.focus();
+    sendMessage("/Send Payment");
+    // editingMessage("/Send Payment");
+    // document.getElementById("message")?.focus();
   };
   const create_cloud = () => {
-    editingMessage("/Create a Cloude Node");
-    document.getElementById("message")?.focus();
+    sendMessage("/Create a Cloude Node");
   };
   const is_this_private = () => {
-    editingMessage("/Is this private?");
-    document.getElementById("message")?.focus();
-    // setEditing(false);
-    // messageHistory.push({
-    //   time_stamp: Date.now(),
-    //   chat: "/Is this private?",
-    //   msg_type: "send_message",
-    //   file: "",
-    // });
-    // setMessageHistory(messageHistory);
-    // scroll_bottom();
+    sendMessage("/Is this private?");
+  };
+
+  const pastImage = async () => {
+    const clipboardItems = await navigator.clipboard.read();
+    for (const clipboardItem of clipboardItems) {
+      console.log(clipboardItem);
+      console.log(clipboardItem.types);
+      const imageTypes = clipboardItem.types.find((type) =>
+        type.startsWith("image/")
+      );
+      const blob = await clipboardItem.getType(imageTypes);
+      console.log(URL.createObjectURL(blob));
+      setFile(URL.createObjectURL(blob));
+      setEditing(true);
+      setMessage(String(Date.now()) + ".png");
+      // Do something with the image blob.
+    }
   };
 
   return (
@@ -154,44 +233,47 @@ export const Feat = () => {
         </div>
       </div>
       <div className="Feat_container" id="HistoryFeild">
-        <div className="Feat_Msg_Image">
-          <img src="/feat/Rectangle.svg" alt="Rectangle" />
+        <div>
+          <div className="Feat_Msg_Image">
+            <img src="/feat/Rectangle.svg" alt="Rectangle" />
+          </div>
+          {messageHistory.map((message, index) => {
+            if (message.msg_type == "recive_message") {
+              return (
+                <div className="Feat_Msg_reciver" key={index}>
+                  <div className="Feat_polygon_reciver">{Icons.Polygon()}</div>
+                  <div className="Feat_chat_reciver">{message.chat}</div>
+                </div>
+              );
+            }
+            if (message.msg_type == "send_message") {
+              return (
+                <div className="Feat_Msg_send" key={index}>
+                  <div className="Feat_chat_send">{message.chat}</div>
+                  <div className="Feat_polygon_send">{Icons.Polygon()}</div>
+                </div>
+              );
+            }
+            if (message.msg_type == "send_image") {
+              return (
+                <div className="Feat_Msg_Image_send" key={index}>
+                  <img src={message.file} alt="Rectangle" />
+                </div>
+              );
+            }
+          })}
         </div>
-        {messageHistory.map((message, index) => {
-          if (message.msg_type == "recive_message") {
-            return (
-              <div className="Feat_Msg_reciver" key={index}>
-                <div className="Feat_polygon_reciver">{Icons.Polygon()}</div>
-                <div className="Feat_chat_reciver">{message.chat}</div>
-              </div>
-            );
-          }
-          if (message.msg_type == "send_message") {
-            return (
-              <div className="Feat_Msg_send" key={index}>
-                <div className="Feat_chat_send">{message.chat}</div>
-                <div className="Feat_polygon_send">{Icons.Polygon()}</div>
-              </div>
-            );
-          }
-          if (message.msg_type == "send_image") {
-            return (
-              <div className="Feat_Msg_Image_send" key={index}>
-                <img src={message.file} alt="Rectangle" />
-              </div>
-            );
-          }
-        })}
       </div>
+      {/* <img src={captureImage} alt="" /> */}
       <div className="Feat_more_tools">
         <button onClick={send_payment}>Send Payment</button>
         <button onClick={create_cloud}>Create a Cloud Node</button>
         <button onClick={is_this_private}>Is this private?</button>
-        {Icons.refreshIcon()}
+        <div onClick={pastImage}>{Icons.refreshIcon()}</div>
       </div>
       <div className="Feat_edit_message">
         {Icons.markIcon()}
-        {!editing ? defaultIcon : sendIcon}
+        {!editing ? defaultIcon : sendIcon()}
       </div>
     </div>
   );
