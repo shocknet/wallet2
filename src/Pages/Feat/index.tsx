8dce2d412d33762f33f92d18087f0d8abf40723a
polarDender <patch.demon115@gmail.com>
2024-08-08 11:59:10 -0700

    feat-complete-ui

--- src/Pages/Feat/index.tsx
+++ src/Pages/Feat/index.tsx
@@ -1,24 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as Icons from "../../Assets/SvgIconLibrary";
 import { useIonRouter } from "@ionic/react";
 
 export const Feat = () => {
-  const messageData = [
+  // const messageData = [
+  //   {
+  //     time_stamp: Date.now(),
+  //     chat: "Hey there, I'm Alice! I'm your wallet assistant and an expert with Bitcoin's Lightning Network... I can set-up a cloud node, open a channel, make a payment, pretty much anything you like... Just ask! So, how can I help you today?",
+  //     msg_type: "recive_message",
+  //     file: "",
+  //   },
+  // ];
+
+  const router = useIonRouter();
+  const [editing, setEditing] = useState(false);
+  const [message, setMessage] = useState("");
+  const [messageHistory, setMessageHistory] = useState([
     {
       time_stamp: Date.now(),
       chat: "Hey there, I'm Alice! I'm your wallet assistant and an expert with Bitcoin's Lightning Network... I can set-up a cloud node, open a channel, make a payment, pretty much anything you like... Just ask! So, how can I help you today?",
       msg_type: "recive_message",
       file: "",
     },
-  ];
-
-  const router = useIonRouter();
-  const [editing, setEditing] = useState(false);
-  const [message, setMessage] = useState("");
-  const [messageHistory, setMessageHistory] = useState(messageData);
+  ]);
   const [file, setFile] = useState("");
+  const [scroll, setScroll] = useState(0);
+
+  useEffect(() => {
+    const scrollField = document.getElementById("HistoryFeild");
+    scrollField?.scrollTo(0, scroll);
+  }, [scroll]);
 
   const sendMessage = () => {
+    const scrollField = document.getElementById("HistoryFeild");
+    setScroll(scrollField.scrollHeight);
     if (file) {
       messageHistory.push({
         time_stamp: Date.now(),
@@ -35,6 +50,7 @@ export const Feat = () => {
       });
     }
     setMessageHistory(messageHistory);
+    // scroll_bottom();
     setEditing(false);
     setMessage("");
     setFile("");
@@ -42,9 +58,20 @@ export const Feat = () => {
 
   const fileDownload = (e: HTMLInputElement) => {
     setEditing(true);
-    setMessage(e.files[0].name);
-    if (e.files[0].type.includes("image")) {
-      setFile(URL.createObjectURL(e.files[0]));
+    const file = e.files[0];
+    setMessage(file.name);
+    if (file.type.includes("image")) {
+      setFile(URL.createObjectURL(file));
+    }
+    document.getElementById("message")?.focus();
+  };
+
+  const editingMessage = (message: string) => {
+    setMessage(message);
+    if (message.length != 0) {
+      setEditing(true);
+    } else {
+      setEditing(false);
     }
   };
 
@@ -56,9 +83,6 @@ export const Feat = () => {
         id="message"
         className="Message"
         onChange={(e) => editingMessage(e.target.value)}
-        onKeyDown={(e) => {
-          console.log(e.key);
-        }}
         value={message}
       />
       <div className="Feat_inputFile">
@@ -98,13 +122,26 @@ export const Feat = () => {
     </div>
   );
 
-  const editingMessage = (message: string) => {
-    setMessage(message);
-    if (message.length != 0) {
-      setEditing(true);
-    } else {
-      setEditing(false);
-    }
+  const send_payment = () => {
+    editingMessage("/Send Payment");
+    document.getElementById("message")?.focus();
+  };
+  const create_cloud = () => {
+    editingMessage("/Create a Cloude Node");
+    document.getElementById("message")?.focus();
+  };
+  const is_this_private = () => {
+    editingMessage("/Is this private?");
+    document.getElementById("message")?.focus();
+    // setEditing(false);
+    // messageHistory.push({
+    //   time_stamp: Date.now(),
+    //   chat: "/Is this private?",
+    //   msg_type: "send_message",
+    //   file: "",
+    // });
+    // setMessageHistory(messageHistory);
+    // scroll_bottom();
   };
 
   return (
@@ -116,24 +153,22 @@ export const Feat = () => {
           <div className="Feat_user_name">Alice</div>
         </div>
       </div>
-      <div className="Feat_container">
+      <div className="Feat_container" id="HistoryFeild">
         <div className="Feat_Msg_Image">
           <img src="/feat/Rectangle.svg" alt="Rectangle" />
         </div>
-        {messageHistory.map((message) => {
+        {messageHistory.map((message, index) => {
           if (message.msg_type == "recive_message") {
             return (
-              <div key={message.time_stamp}>
-                <div className="Feat_Msg_reciver">
-                  <div className="Feat_polygon_reciver">{Icons.Polygon()}</div>
-                  <div className="Feat_chat_reciver">{message.chat}</div>
-                </div>
+              <div className="Feat_Msg_reciver" key={index}>
+                <div className="Feat_polygon_reciver">{Icons.Polygon()}</div>
+                <div className="Feat_chat_reciver">{message.chat}</div>
               </div>
             );
           }
           if (message.msg_type == "send_message") {
             return (
-              <div className="Feat_Msg_send" key={message.time_stamp}>
+              <div className="Feat_Msg_send" key={index}>
                 <div className="Feat_chat_send">{message.chat}</div>
                 <div className="Feat_polygon_send">{Icons.Polygon()}</div>
               </div>
@@ -141,19 +176,17 @@ export const Feat = () => {
           }
           if (message.msg_type == "send_image") {
             return (
-              <div key={message.time_stamp}>
-                <div className="Feat_Msg_Image_send">
-                  <img src={message.file} alt="Rectangle" />
-                </div>
+              <div className="Feat_Msg_Image_send" key={index}>
+                <img src={message.file} alt="Rectangle" />
               </div>
             );
           }
         })}
       </div>
       <div className="Feat_more_tools">
-        <button>Send Payment</button>
-        <button>Create a Cloud Node</button>
-        <button>Is this private?</button>
+        <button onClick={send_payment}>Send Payment</button>
+        <button onClick={create_cloud}>Create a Cloud Node</button>
+        <button onClick={is_this_private}>Is this private?</button>
         {Icons.refreshIcon()}
       </div>
       <div className="Feat_edit_message">
