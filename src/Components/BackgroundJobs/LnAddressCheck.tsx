e7f51c287115dac06ea37ba413640d8588f74bcd
Mothana <snakeeaterrdev@gmail.com>
2024-10-17 21:32:23 +0400

    fix

--- src/Components/BackgroundJobs/LnAddressCheck.tsx
+++ src/Components/BackgroundJobs/LnAddressCheck.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch } from "../../State/store";
 
 import { upgradeSourcesToNofferBridge } from "../../State/bridgeMiddleware";
+const nofferedLnStorageKey = "noffer" // Has this wallet instance upgraded to new bridge
 
 export const LnAddressCheck = () => {
 
@@ -9,7 +10,12 @@ export const LnAddressCheck = () => {
 
 
 	useEffect(() => {
+		const nofferied = localStorage.getItem(nofferedLnStorageKey)
+		if (nofferied) {
+			return
+		}
 		dispatch(upgradeSourcesToNofferBridge())
+		localStorage.setItem(nofferedLnStorageKey, "true")
 	}, [dispatch]);
 
 	return null;
