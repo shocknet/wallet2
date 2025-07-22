0732946f768491a096dd29daf4a0fd926dd8c836
boufni95 <boufni95@gmail.com>
2025-07-22 18:57:24 +0000

    up

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
       workbox: {
         globPatterns: [],
         globIgnores: ['*'],
+        cleanupOutdatedCaches: true,
         maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
       },
     }),
