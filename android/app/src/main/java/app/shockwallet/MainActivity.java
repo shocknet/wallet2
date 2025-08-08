42e7fa53ee3113459ef20eaea512d9607b410c70
Mothana <snakeeaterrdev@gmail.com>
2025-08-08 19:44:48 +0400

    forward logging to mainActivity

--- android/app/src/main/java/app/shockwallet/MainActivity.java
+++ android/app/src/main/java/app/shockwallet/MainActivity.java
@@ -1,6 +1,29 @@
 package app.shockwallet;
 
+import android.os.Bundle;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		getBridge().getWebView().setWebChromeClient(new WebChromeClient() {
+			@Override
+			public boolean onConsoleMessage(ConsoleMessage m) {
+				String line = "WV " + m.messageLevel() + ": " + m.message()
+						+ " (" + m.sourceId() + ":" + m.lineNumber() + ")";
+
+				// Use System.out/err so R8/ProGuard can't strip it
+				if (m.messageLevel() == ConsoleMessage.MessageLevel.ERROR) {
+					System.err.println(line);
+				} else {
+					System.out.println(line);
+				}
+				return true; // we handled it
+			}
+		});
+	}
 }
