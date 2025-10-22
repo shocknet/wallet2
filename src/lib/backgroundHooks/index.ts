018c94c6408b66381d5b0019512279fe27e24674
Mothana <snakeeaterrdev@gmail.com>
2025-10-22 18:39:36 +0400

    bring back push notifs

--- src/lib/backgroundHooks/index.ts
+++ src/lib/backgroundHooks/index.ts
@@ -2,14 +2,14 @@ import { useAppLifecycle } from "./useAppLifecycle";
 import { useDebitRequestHandler } from "./useDebitRequestHandler"
 /* import { useLnAddressCheck } from "./useLnAddressCheck"; */
 import { useManageRequestHandler } from "./useManageRequestHandler";
-/* import { usePush } from "./usePush"; */
+import { usePush } from "./usePush";
 /* import { useSubscriptionsBackground } from "./useSubscriptionsBackground"; */
 /* import { useSubToBeacons } from "./useSubToBeacons"; */
 
 const BackgroundJobs = () => {
 
 	useDebitRequestHandler();
-	/* usePush(); */
+	usePush();
 	/* useLnAddressCheck(); */
 	useManageRequestHandler();
 	/* useSubscriptionsBackground(); */
