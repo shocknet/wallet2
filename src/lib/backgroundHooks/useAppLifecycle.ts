86468b5b3c8758bc705a061736dfdd069bb2fb80
Mothana <snakeeaterrdev@gmail.com>
2025-12-03 19:54:24 +0400

    alert to make perm request a user guesture callback

--- src/lib/backgroundHooks/useAppLifecycle.ts
+++ src/lib/backgroundHooks/useAppLifecycle.ts
@@ -1,20 +1,16 @@
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import { App } from "@capacitor/app";
-import { useDispatch, useSelector } from "@/State/store/store";
+import { useDispatch } from "@/State/store/store";
 import { useAlert } from "../contexts/useAlert";
-import { usePreference } from "../hooks/usePreference";
-import { isPlatform } from "@ionic/react";
+
+import { initLocalNotifications } from "../local-notifications";
 
 
-const DENIED_NOTIFICATIONS_PERMISSIONS = "notif_perms_denied";
+
 
 
 export const useAppLifecycle = () => {
 	const dispatch = useDispatch();
-
-	const nodedUp = useSelector(state => state.appState.bootstrapped);
-
-	const { cachedValue, setValue, isLoaded } = usePreference<boolean>(DENIED_NOTIFICATIONS_PERMISSIONS, false);
 
 	const { showAlert } = useAlert();
 
@@ -22,92 +18,15 @@ export const useAppLifecycle = () => {
 
 
 	useEffect(() => {
-		console.log("App started: Running initial setup...");
 
-		const tasks = () => {
 
-		}
-
-		// App start
-		const appStartUp = async () => {
-			if (nodedUp) { // tasks that should only take place after noding up
-				tasks();
-			}
-		}
-		appStartUp();
+		initLocalNotifications(showAlert);
 
 
 		// App resume
 		const onAppResume = async () => {
 			console.log("App resumed");
-			if (nodedUp) { // tasks that should only take place after noding up
-				tasks()
-			}
-		};
-
-		// App pause
-		const onAppPause = () => {
-			console.log("App paused.");
-
-		};
-
-		const listener = App.addListener("appStateChange", (state) => {
-			if (state.isActive) {
-				onAppResume();
-			} else {
-				onAppPause();
-			}
-		});
-
-		return () => {
-			listener.then(h => h.remove());
-		};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [dispatch, nodedUp]);
-
-
-
-	/* Handle notifications */
-	const handleInitNotifications = useCallback(async () => {
-		if (cachedValue) {
-			return;
-		}
-
-		let initLocalNotifications;
-		try {
-			({ initLocalNotifications } = await import("@/lib/local-notifications"))
-		} catch (err) {
-			console.error('Failed to lazy-load "@/lib/local-notifications"', err);
-			return;
-		}
-
-		const hasPermission = await initLocalNotifications();
-		if (!hasPermission) {
-			showAlert({
-				header: "Notifications Disabled",
-				message: !isPlatform("hybrid")
-					? 'You have blocked notifications in your browser. Please enable them in your browser settings if you wish to receive notifications.'
-					: 'Notifications are disabled. Please enable them in your device settings to receive alerts.',
-				buttons: ['OK']
-			});
-		}
-		setValue(true);
-	}, [cachedValue, setValue, showAlert]);
-
-	useEffect(() => {
-
-
-		if (isLoaded) {
-			handleInitNotifications();
-		}
-
-
-		// App resume
-		const onAppResume = async () => {
-			console.log("App resumed");
-			if (isLoaded) {
-				await handleInitNotifications();
-			}
+			initLocalNotifications(showAlert);
 		};
 
 		// App pause
@@ -127,6 +46,6 @@ export const useAppLifecycle = () => {
 		return () => {
 			listener.then(h => h.remove());
 		};
-	}, [dispatch, isLoaded, handleInitNotifications])
+	}, [dispatch, showAlert])
 };
 
