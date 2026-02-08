c6372870a2e3f4fc9e5cb25e8499b4eb622d4521
Mothana <snakeeaterrdev@gmail.com>
2026-02-08 19:37:41 +0400

    remove alert

--- src/notifications/push/capture.ts
+++ src/notifications/push/capture.ts
@@ -79,7 +79,6 @@ export function captureNativeEarly() {
 
 	PushNotifications.addListener("pushNotificationActionPerformed", (action) => {
 		console.log("[Push] Native notification tapped:", action);
-		alert(JSON.stringify(action));
 
 
 		const d: any = action.notification?.data;
