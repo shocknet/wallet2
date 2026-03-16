2fa533102f1113008888d301fb1bbce4da1d53c6
Mothana <snakeeaterrdev@gmail.com>
2026-03-16 22:44:16 +0400

    fixed dynamic module import error after new deploys

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,17 @@ import { registerRootLifecycle } from './State/runtime/lifecycle';
 import { initLocalNotifications } from './notifications/local/local-notifications';
 import { initPushNotifications } from './notifications/push/init';
 
+/*
+	A web user visiting new build after having used an older build
+	may run into dynamic import errors when navigating to lazily loaded pages.
+	See: https://vite.dev/guide/build#load-error-handling
+*/
+window.addEventListener('vite:preloadError', (event) => {
+	event.preventDefault();
+	window.location.reload();
+});
+
+
 registerRootLifecycle();
 
 initPushNotifications();
