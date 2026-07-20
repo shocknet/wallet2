ef8f123edd4ff2b0baecacffdde2084cdcfb012f
Mothana <snakeeaterrdev@gmail.com>
2026-07-20 18:39:50 +0400

    test

--- src/main.tsx
+++ src/main.tsx
@@ -2,69 +2,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import bootstrapShockwallet from './bootstrap';
-import { registerSW } from 'virtual:pwa-register';
-import { Capacitor } from '@capacitor/core';
 
-const registrations =
-	await navigator.serviceWorker.getRegistrations();
 
-console.table(
-	registrations.map(registration => ({
-		scope: registration.scope,
-		active: registration.active?.scriptURL,
-		activeState: registration.active?.state,
-		waiting: registration.waiting?.scriptURL,
-		installing: registration.installing?.scriptURL,
-		updateViaCache: registration.updateViaCache,
-	}))
-);
-
-console.log(
-	"Current controller:",
-	navigator.serviceWorker.controller?.scriptURL,
-);
-
-if (!Capacitor.isNativePlatform()) {
-	console.log("registering service worker");
-	registerSW({
-		immediate: true,
-
-		onRegisteredSW(swScriptUrl, registration) {
-			console.log("[PWA] registered", {
-				swScriptUrl,
-				active: registration?.active?.scriptURL,
-				waiting: registration?.waiting?.scriptURL,
-				installing: registration?.installing?.scriptURL,
-			});
-
-			registration?.addEventListener("updatefound", () => {
-				console.log("[PWA] update found");
-
-				const worker = registration.installing;
-
-				worker?.addEventListener("statechange", () => {
-					console.log("[PWA] worker state:", worker.state);
-				});
-			});
-		},
-
-		onNeedRefresh() {
-			console.log("[PWA] onNeedRefresh called");
-		},
-
-		onOfflineReady() {
-			console.log("[PWA] offline ready");
-		},
-
-		onRegisterError(error) {
-			console.error("[PWA] registration failed", error);
-		},
-	});
-
-	navigator.serviceWorker.addEventListener("controllerchange", () => {
-		console.log("[PWA] controller changed");
-	});
-}
 
 
 
