e13a3119999c88a0097908936dda57a56ea3e059
Mothana <snakeeaterrdev@gmail.com>
2026-07-20 18:14:49 +0400

    test

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,50 @@ import App from './App';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import bootstrapShockwallet from './bootstrap';
 import { registerSW } from 'virtual:pwa-register';
+import { Capacitor } from '@capacitor/core';
 
+if (!Capacitor.isNativePlatform()) {
+	console.log("registering service worker");
+	registerSW({
+		immediate: true,
 
-registerSW({
-	immediate: true,
-	onRegisteredSW(swScriptUrl, registration) {
-		console.info('Service worker registered', swScriptUrl, registration);
-	},
-	onOfflineReady() {
-		console.log('App is completely cached and ready to work offline.');
-	}
-});
+		onRegisteredSW(swScriptUrl, registration) {
+			console.log("[PWA] registered", {
+				swScriptUrl,
+				active: registration?.active?.scriptURL,
+				waiting: registration?.waiting?.scriptURL,
+				installing: registration?.installing?.scriptURL,
+			});
+
+			registration?.addEventListener("updatefound", () => {
+				console.log("[PWA] update found");
+
+				const worker = registration.installing;
+
+				worker?.addEventListener("statechange", () => {
+					console.log("[PWA] worker state:", worker.state);
+				});
+			});
+		},
+
+		onNeedRefresh() {
+			console.log("[PWA] onNeedRefresh called");
+		},
+
+		onOfflineReady() {
+			console.log("[PWA] offline ready");
+		},
+
+		onRegisterError(error) {
+			console.error("[PWA] registration failed", error);
+		},
+	});
+
+	navigator.serviceWorker.addEventListener("controllerchange", () => {
+		console.log("[PWA] controller changed");
+	});
+}
+
 
 
 bootstrapShockwallet();
