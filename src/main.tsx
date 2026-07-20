58953c10aa67fbcedea8b1219cbe4f16e0f7a0b6
Mothana <snakeeaterrdev@gmail.com>
2026-07-20 16:13:49 +0400

    pwa service worker automatic reload

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
+import { registerSW } from 'virtual:pwa-register';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import bootstrapShockwallet from './bootstrap';
+import { Capacitor } from '@capacitor/core';
 
 
-
+if (!Capacitor.isNativePlatform()) {
+	registerSW({ immediate: true });
+}
 
 bootstrapShockwallet();
-
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-pwa/vanillajs" />
 declare const __WEB_APP_VERSION__: string;
 declare const __WEB_APP_VERSION_CODE__: string;
