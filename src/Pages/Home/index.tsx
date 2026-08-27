4298112e31d71092c9d0e2ccaa31c9b4babe58ac
Mothana <snakeeaterrdev@gmail.com>
2026-08-27 15:22:23 +0400

    safe area for the fixed slotted home cta bar

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -247,7 +247,7 @@ const Home = () => {
 				}
 				<div
 					slot="fixed"
-					className="pointer-events-none absolute inset-x-0 bottom-0 z-10 px-3"
+					className="pointer-events-none absolute inset-x-0 bottom-0 z-10 px-3 pb-[var(--ion-safe-area-bottom,0px)]"
 				>
 
 					<div
