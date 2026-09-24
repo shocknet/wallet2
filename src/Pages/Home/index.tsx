b2dc0555720457c1c14901399e115bf6e3fbf1b1
Mothana <snakeeaterrdev@gmail.com>
2026-09-24 21:03:57 +0400

    test

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -143,6 +143,7 @@ const Home = () => {
 				<BalanceCard />
 			</IonHeader>
 			<IonContent scrollY={false} className="ion-content-no-footer">
+				<div>hello test</div>
 				<IonRefresher slot="fixed" onIonRefresh={handleRefresh}>
 					<IonRefresherContent></IonRefresherContent>
 				</IonRefresher>
